e81b6f2dfa1a6e88ca7df10534e2e6e1912e7caa
Roman Koshchei <romankoshchei@gmail.com>
2023-07-10 22:05:42 +0300

    init

---
+++ Data/Db.cs
@@ -0,0 +1,12 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class Db : IdentityDbContext<User>
+{
+    public Db(DbContextOptions options) : base(options)
+    {
+    }
+}
