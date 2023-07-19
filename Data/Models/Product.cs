1ae290344be7fcf94c5be4078dfc07298c672789
Roman Koshchei <romankoshchei@gmail.com>
2023-07-19 17:03:57 +0300

    sync

--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -8,21 +8,24 @@ namespace Data.Models
 {
     public class Product
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
-        public double Price { get; set; }
-        public int Amount { get; set; }
+        public double Price { get; set; } = 0;
+        public int Amount { get; set; } = 0;
         public string PreviewImage { get; set; }
         public List<ProductImage> Images { get; set; }
+
         //public List<CategoryToProduct> Categories?
         public string ShopId { get; set; }
+
         public Shop Shop { get; set; }
-        public string VideoURL { get; set; }
+
+        public string? VideoUrl { get; set; }
+
         //SEO
-        public string SeoTitle { get; set; }
-        public string SeoDescription { get; set; }
-        public string SeoSlug { get; set; }
+        public string SeoTitle { get; set; } = string.Empty;
 
-
+        public string SeoDescription { get; set; } = string.Empty;
+        public string SeoSlug { get; set; } = string.Empty;
     }
 }
