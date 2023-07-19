810ce60ec8cd6dea09427d14a2d8b676227b4ec4
DmitryFlud2003 <valvech322@gmail.com>
2023-07-19 13:40:38 +0300

    Created models

---
+++ Data/Models/Banner.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Banner
+    {
+        public string URL { get; set; }
+        public string ShopId { get; set; }
+    }
+}
