3df50c37946761e91dad125129e8406ebfd1cc3b
DmitryFlud2003 <valvech322@gmail.com>
2023-07-28 09:55:47 +0300

    Update DashboardController

--- Backend/Controllers/DashboardController.cs
+++ Backend/Controllers/DashboardController.cs
@@ -1,10 +1,14 @@
-﻿using Data;
+﻿using Backend.Services;
+using Data;
+using Data.Models.ProductTables;
 using Data.Models.ShopTables;
 using Lib;
 using Lib.EntityFrameworkCore;
+using Lib.Storage;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Backend.Controllers;
 
@@ -12,11 +16,19 @@ namespace Backend.Controllers;
 [ApiController]
 public class DashboardController : ControllerBase
 {
+   
+    private readonly IStorage storage;
     private readonly Db db;
+    private readonly ImageService imageService;
+    private readonly BackgroundQueue background;
 
-    public DashboardController(Db db)
+    public DashboardController( IStorage storage, Db db, ImageService imageService, BackgroundQueue background)
     {
+      
+        this.storage = storage;
         this.db = db;
+        this.imageService = imageService;
+        this.background = background;
     }
 
     public record PagesOutput(string Slug, string Title, DateTime UpdatedAt);
@@ -123,4 +135,104 @@ public class DashboardController : ControllerBase
         var saved = await db.Save();
         return saved ? Ok(page) : Problem();
     }
+
+    [HttpDelete("{shopId}")]
+    [Authorize]
+    public async Task<IActionResult> DeleteShop([FromRoute] string shopId)
+    {
+        var uid = User.FindFirst(Jwt.Uid);
+        if (uid == null) return Unauthorized();
+
+        var products = await db.Products
+            .Where(x => x.ShopId == shopId)
+            .Include(p=>p.Images)
+            .Include(p=>p.ProductCategories)
+            .QueryMany();
+        if (products != null) {
+
+            foreach (var product in products)
+            {
+                await imageService.SafeDelete(product.Images);
+
+                db.Products.Remove(product);
+
+                var isSaved = await db.Save();
+                if (!isSaved) return Problem();
+               
+            }
+        }
+
+        var shop = await db.Shops
+            .Where(s => s.Id == shopId)
+            .Include(s => s.Banners)
+            .Include(s => s.InfoPages)
+            .Include(s => s.SocialMediaLinks)
+            .QueryOne();
+
+        if (shop != null)
+        {
+            await imageService.SafeDelete(shop.Banners);
+            db.Shops.Remove(shop);
+            var isSaved = await db.Save();
+            if (!isSaved) return Problem();
+       
+        }
+
+
+        var saved = await db.Save();
+        return saved ? Ok() : Problem();
+    }
+
+    [HttpPut("{shopName}/shop")]
+    [Authorize]
+    public async Task<IActionResult> AddShop([FromRoute] string shopName, IFormFile file)
+    {
+        var uid = User.FindFirst(Jwt.Uid);
+        if (uid == null) return Unauthorized();
+
+        var key = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+        var upload = await storage.Upload(key, file.OpenReadStream());
+        if (upload == null) return Problem();
+
+        var link = storage.Url(upload);
+        if (link == null) return Problem();
+
+        var shop = new Shop(shopName, link, uid.Value);
+
+        await db.Shops.AddAsync(shop);
+
+        var saved = await db.Save();
+        return saved ? Ok() : Problem();
+    }
+
+
+
+    [HttpGet("shops")]
+    [Authorize]
+    public async Task<IActionResult> Shops()
+    {
+        var uid = User.FindFirst(Jwt.Uid);
+        if (uid == null) return Unauthorized();
+
+        var shops = await db.Shops
+            .Where(x => x.OwnerId == uid.Value)
+            .QueryMany();
+        
+        return Ok(shops);
+    }
+
+    /*[HttpGet("shops/filter/{search}")]
+    [Authorize]
+    public async Task<IActionResult> FilterShops([FromRoute] string search)
+    {
+        var uid = User.FindFirst(Jwt.Uid);
+        if (uid == null) return Unauthorized();
+
+        var shops = await db.Shops
+            .Where(x => x.OwnerId == uid.Value && x.Name.Contains(search))
+            .QueryMany();
+
+        return Ok(shops);
+    }*/
 }
